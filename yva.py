7f97d67540697e81323bff644ddf8d134f611f4d
Martin Stensgård <mastensg@ping.uio.no>
2010-09-27 13:47:50 +0200

    Refactoring.

--- yva.py
+++ yva.py
@@ -10,6 +10,15 @@ import pygame
 #import cProfile
 #import pstats
 
+framerate = 60
+
+tile_width = 32
+tile_height = 32
+
+window_width = 640
+window_height = 480
+editor_width = 4 * tile_width
+
 class Camera:
     x = 0
 
@@ -36,56 +45,19 @@ class Player:
 draw_tile = lambda color, x, y: pygame.draw.rect(screen, color,
         (x * tile_width, y * tile_height, tile_width, tile_height))
 
-def main():
-    # Check for editing mode.
-    editing = "-e" in sys.argv
-
-    # Gravity.
-    g = .5
-
-    # Screen size.
-    screen_width = 640
-    screen_height = 480
-
-    # Tiles.
-    tile_width = 32
-    tile_height = 32
-    tile_image = pygame.image.load("gfx/tiles.png")
+def load_tiles(filename):
+    tile_image = pygame.image.load(filename)
 
     tiles = []
     for y in range(tile_image.get_rect()[3] // tile_height):
         tiles.append(tile_image.subsurface(0, y * tile_height, tile_width, tile_height))
 
-    current_tile = 0
+    return tiles
 
-    # Level.
-    level_filename = sys.argv[-1]
-
-    if len(sys.argv) > 1 and os.path.isfile(level_filename):
-        layernames, tilemap = level = eval(open(level_filename).read())
-    else:
-        if not editing:
-            if len(sys.argv) == 2:
-                print "Level \"%s\" does not exist. Try creating a level using the editor option (-e)." % level_filename
-            print "Usage: %s [-e] level" % sys.argv[0]
-            sys.exit(1)
-
-        layernames = [None, None,
-                "mountains_1.png", None,
-                "mountains_2.png", None,
-                "mountains_3.png", None,
-                None, None, "heaven.png"]
-
-        tilemap = []
-        for i in range(14):
-            tilemap.append([0 for i in range(3 * screen_width // tile_width)])
-        tilemap.append([1 for i in range(3 * screen_width // tile_width)])
-
-        level = layernames, tilemap
-
+def play(level, window, tiles, editing=False):
+    layernames, tilemap = level
     level_width = len(tilemap[0]) * tile_width
 
-    # Layers.
     layers = []
     for layername in layernames:
         if layername:
@@ -94,60 +66,29 @@ def main():
         else:
             layers.append(None)
 
-    # Colors.
-    bgcolor = 0, 127, 0
-    fgcolor = 255, 255, 255
-    hgcolor = 255, 0, 0
-    mgcolor = 255, 127, 0
+    current_tile = 0
+    g = .5
 
-    # Screen surface and size.
-    editor_width = 4 * tile_width
-
-    if editing:
-        window_width = screen_width + editor_width
-    else:
-        window_width = screen_width
-
-    window_height = screen_height
-
-    window = pygame.display.set_mode((window_width, window_height))
-
-    if editing:
-        screen = window.subsurface((editor_width, 0, screen_width, screen_height))
-    else:
-        screen = window.subsurface((0, 0, screen_width, screen_height))
-
-    # Game objects.
     camera = Camera()
     player = Player()
 
-    # Short buffer for low latency.
-    pygame.mixer.pre_init(buffer=512)
-
-    # Initialize all pygame submodules.
-    pygame.init()
-
-    # Generate display update request at the framerate.
-    framerate = 60 # Frames per second.
-    pygame.time.set_timer(pygame.VIDEOEXPOSE, 1000 / framerate)
-
-    # Load sound effects.
-    #bip = pygame.mixer.Sound("bip.wav")
-    #bop = pygame.mixer.Sound("bop.wav")
-    #end = pygame.mixer.Sound("end.wav")
+    screen_height = window_height
+    if editing:
+        screen_width = window_width - editor_width
+        screen = window.subsurface((editor_width, 0, screen_width, screen_height))
+    else:
+        screen_width = window_width
+        screen = window.subsurface((0, 0, screen_width, screen_height))
 
     while True:
-        # Wait until event arrives.
         event = pygame.event.wait()
 
-        # If we should update the screen...
         if event.type == pygame.VIDEOEXPOSE:
             # Check if the player is falling onto a platform.
             # x and y are measured in tiles.
 
             # Check if the tiles covered by the player are filled in.
             if player.vy > 0:
-                # Find the height of the tiles under the player's feet.
                 y = int((player.y + player.h + player.vy) // tile_height)
 
                 for x in range(player.x // tile_width, (player.x + player.w - 1) // tile_width + 1):
@@ -161,7 +102,6 @@ def main():
                             player.vy = 0
                             player.airborne = False
 
-                    #draw_tile(fgcolor, x, y)
             elif player.vy < 0:
                 # Find the height of the tiles over the player's head.
                 y = int(player.y // tile_height)
@@ -172,8 +112,6 @@ def main():
                         player.y = y * tile_height + player.h - g
                         player.vy = 0
 
-                    #draw_tile(fgcolor, x, y)
-
             # Stop player if trying to walk into a wall.
             if player.vx > 0:
                 x = (player.x + player.w) // tile_width
@@ -183,8 +121,6 @@ def main():
                         player.x -= player.vx
                         break
 
-                    #draw_tile(hgcolor, x, y)
-
             elif player.vx < 0:
                 x = (player.x + player.w - 1) // tile_width - 1
 
@@ -192,8 +128,6 @@ def main():
                     if y in range(len(tilemap)) and tilemap[y][x]:
                         player.x -= player.vx
                         break
-
-                    #draw_tile(hgcolor, x, y)
 
             # Move the player.
             if player.jumptime > -1:
@@ -218,7 +152,6 @@ def main():
                 camera.x = player.x - screen_width // 2
 
             # Draw layers.
-            screen.fill(bgcolor)
             for i in range(len(layers) - 1, -1, -1):
                 if layers[i]:
                     x = -camera.x // (1 << i)
@@ -253,7 +186,6 @@ def main():
             # Swap front and back buffers.
             pygame.display.update()
 
-        # If a key is pressed...
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 player.vx -= player.speed
@@ -284,7 +216,6 @@ def main():
                         row.pop()
                         level_width = len(tilemap[0]) * tile_width
 
-        # If a key is released...
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 player.vx += player.speed
@@ -297,7 +228,6 @@ def main():
                 if player.vy < 0:
                     player.vy += player.agility / 2
 
-        # If a mouse button is clicked...
         elif editing and event.type == pygame.MOUSEBUTTONDOWN:
             if event.pos[0] > editor_width:
                 x = (event.pos[0] - editor_width + camera.x) // tile_width
@@ -320,7 +250,6 @@ def main():
                     else:
                         x += tile_width
 
-        # If a mouse button is held down...
         elif editing and event.type == pygame.MOUSEMOTION:
             if event.pos[0] > editor_width:
                 x = (event.pos[0] - editor_width + camera.x) // tile_width
@@ -329,6 +258,45 @@ def main():
                     tilemap[y][x] = current_tile
                 elif event.buttons[2]:
                     tilemap[y][x] = 0
+
+    return level
+
+def main():
+    editing = "-e" in sys.argv
+
+    level_filename = sys.argv[-1]
+
+    if len(sys.argv) > 1 and os.path.isfile(level_filename):
+        level = eval(open(level_filename).read())
+    else:
+        if not editing:
+            if len(sys.argv) == 2:
+                print "Level \"%s\" does not exist. Try creating a level using the editor option (-e)." % level_filename
+
+            print "Usage: %s [-e] level" % sys.argv[0]
+            sys.exit(1)
+
+        layernames = [None, None,
+                "mountains_1.png", None,
+                "mountains_2.png", None,
+                "mountains_3.png", None,
+                None, None, "heaven.png"]
+
+        tilemap = []
+        for i in range(14):
+            tilemap.append([0 for i in range(3 * screen_width // tile_width)])
+        tilemap.append([1 for i in range(3 * screen_width // tile_width)])
+
+        level = layernames, tilemap
+
+    # Short buffer for low latency.
+    pygame.mixer.pre_init(buffer=512)
+
+    pygame.init()
+    pygame.time.set_timer(pygame.VIDEOEXPOSE, 1000 / framerate)
+    window = pygame.display.set_mode((window_width, window_height))
+
+    play(level, window, load_tiles("gfx/tiles.png"), editing=editing)
 
     if editing:
         f = open(level_filename, "w")
